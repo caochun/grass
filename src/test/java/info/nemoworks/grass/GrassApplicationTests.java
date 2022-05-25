817832a8cfe0b62db9ee7a6f3e82657175b511f2
Chun <caochun@gmail.com>
2022-05-25 21:33:46 +0800

    init

---
+++ src/test/java/info/nemoworks/grass/GrassApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nemoworks.grass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
