12c868080abf5fda9124e27f4bfff0addcb5c26c
Chun <caochun@gmail.com>
2022-05-26 22:42:00 +0800

    ecore

--- src/main/java/info/nemoworks/grass/GrassApplication.java
+++ src/main/java/info/nemoworks/grass/GrassApplication.java
@@ -1,7 +1,14 @@
 package info.nemoworks.grass;
 
-import info.nemoworks.grass.meta.GType;
 import info.nemoworks.grass.storage.arcadedb.ArcadeRepository;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -21,7 +28,16 @@ public class GrassApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        arcadeRepository.saveClass(GType.builder().name("abc1").build());
-        arcadeRepository.getClass("abc1");
+        ResourceSet resourceSet = new ResourceSetImpl();
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+//        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new XMIResourceFactoryImpl());
+        EcorePackage ecorePackage = EcorePackage.eINSTANCE;
+
+        URI uri = URI.createFileURI("src/main/resources/bowling.ecore");
+
+        Resource myMetaModel = resourceSet.getResource(uri, true);
+//        EPackage univEPackage = (EPackage) myMetaModel.getContents().get(0);
+        System.out.println(myMetaModel);
+
     }
 }
