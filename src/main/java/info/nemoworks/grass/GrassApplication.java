93bbda31df09e1768538a6eed380ae8e00c1a36c
Chun <caochun@gmail.com>
2022-05-28 00:16:24 +0800

    Almost there

--- src/main/java/info/nemoworks/grass/GrassApplication.java
+++ src/main/java/info/nemoworks/grass/GrassApplication.java
@@ -1,96 +1,19 @@
 package info.nemoworks.grass;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.*;
-import org.eclipse.emf.ecore.impl.EAttributeImpl;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.emf.ecore.xml.type.SimpleAnyType;
-import org.eclipse.emf.ecore.xml.type.XMLTypeFactory;
-import org.eclipse.emfcloud.jackson.databind.EMFContext;
-import org.eclipse.emfcloud.jackson.module.EMFModule;
-import org.eclipse.emfcloud.jackson.resource.JsonResourceFactory;
+import info.nemoworks.grass.core.GModel;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class GrassApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
-        ResourceSet resourceSet = new ResourceSetImpl();
-        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+        GModel gModel = new GModel("src/main/resources/bowling.ecore", "http://org/eclipse/example/bowling");
 
-        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
-        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+        System.out.println(gModel.getClassNames());
 
-        EcorePackage.eINSTANCE.eClass();
+        System.out.println(gModel.getReferenceNames("Game"));
 
-        // Get the URI of the model file.
-        URI fileURI = URI.createFileURI("/Users/chun/Downloads/bowling.ecore");
-
-        // Demand load the resource for this file.
-        Resource resource = resourceSet.getResource(fileURI, true);
-
-
-        EPackage bowlingEPackage = (EPackage) resource.getContents().get(0);
-
-
-        EClass playerEClass = (EClass) bowlingEPackage.getEClassifier("Player");
-
-        resourceSet.getPackageRegistry().put("http://org/eclipse/example/bowling", bowlingEPackage);
-
-        EFactory bowlingInstance = bowlingEPackage.getEFactoryInstance();
-
-        EObject playerObject = bowlingInstance.create(playerEClass);
-
-        EList<EStructuralFeature> eStructuralFeatures =  ((EClass) bowlingEPackage.getEClassifier("Player")).getEStructuralFeatures();
-
-        for(EStructuralFeature eStructuralFeature: eStructuralFeatures){
-            if (eStructuralFeature instanceof EAttribute){
-                if  (((EAttribute)eStructuralFeature).getEAttributeType().getInstanceClassName().equals("java.lang.String")){
-//                    SimpleAnyType wrapper = XMLTypeFactory.eINSTANCE.createSimpleAnyType();
-//                    wrapper.setInstanceType(((EAttribute)eStructuralFeature).getEAttributeType());
-//                    wrapper.setValue("ABC");
-                    playerObject.eSet(eStructuralFeature, "ABC");
-                }
-
-
-            }
-        }
-
-
-        ObjectMapper mapper = new ObjectMapper();
-        EMFModule module = new EMFModule();
-        mapper.registerModule(module);
-        JsonResourceFactory factory = new JsonResourceFactory(mapper);
-
-        String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resource);
-
-        String objectString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(playerObject);
-
-        System.out.println(jsonString);
-
-        System.out.println(objectString);
-
-        String str = "{\"eClass\" : \"file:/Users/chun/Downloads/bowling.ecore#//Player\",\"name\" : \"ABC\", \"height\" : 5.0 }";
-
-        JsonNode actualObj = mapper.readTree(str);
-
-        Object user = mapper.reader()
-                .withAttribute(EMFContext.Attributes.RESOURCE, resource)
-                .forType(Object.class)
-                .readValue(actualObj);
-
-        System.out.println(user);
 
     }
 
---
+++ src/main/java/info/nemoworks/grass/core/GModel.java
@@ -0,0 +1,111 @@
+package info.nemoworks.grass.core;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.*;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emfcloud.jackson.databind.EMFContext;
+import org.eclipse.emfcloud.jackson.module.EMFModule;
+import org.eclipse.emfcloud.jackson.resource.JsonResourceFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GModel {
+
+    private EPackage ePackage;
+    private Resource resource;
+
+    private ObjectMapper mapper;
+
+    private ObjectMapper getMapper() {
+        if (mapper == null)
+            mapper = new ObjectMapper();
+        return mapper;
+    }
+
+    public GModel(String modelUri, String modelNs) throws Exception {
+
+        ResourceSet resourceSet = new ResourceSetImpl();
+
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+
+        EcorePackage.eINSTANCE.eClass();
+
+        // Get the URI of the model file.
+        URI fileURI = URI.createFileURI(modelUri);
+
+        // Demand load the resource for this file.
+        Resource resource = resourceSet.getResource(fileURI, true);
+
+        if (resource == null) {
+            throw new Exception("Resource creation fails");
+        }
+
+        this.resource = resource;
+
+        this.ePackage = (EPackage) resource.getContents().get(0);
+
+        if (this.ePackage == null) {
+            throw new Exception("Model initialization fails.");
+        }
+        resourceSet.getPackageRegistry().put(modelNs, ePackage);
+
+    }
+
+    public EClass getEClass(String name) {
+        EClassifier eClassifier = this.ePackage.getEClassifier(name);
+        if (eClassifier instanceof EClass) return (EClass) eClassifier;
+        return null;
+    }
+
+    public List<EClass> getEClasses() {
+        EList<EClassifier> eClassifiers = this.ePackage.getEClassifiers();
+        return eClassifiers.stream().filter(c -> c instanceof EClass).map(c -> (EClass) c).collect(Collectors.toList());
+    }
+
+    public List<String> getClassNames() {
+        return this.getEClasses().stream().map(c -> c.getName()).collect(Collectors.toList());
+    }
+
+    public List<EReference> getReferences(EClass eClass) {
+        return eClass.getEAllReferences();
+    }
+
+    public List<String> getReferenceNames(String className) {
+        return getReferences(this.getEClass(className)).stream().map(r -> r.getName()).collect(Collectors.toList());
+    }
+
+//    public List<EReference> getAllReferences() {
+//        return this.getClasses().stream().flatMap(eClass -> Stream.of(eClass.getEAllReferences())).;
+//    }
+
+    public String toJson() throws JsonProcessingException {
+        EMFModule module = new EMFModule();
+        getMapper().registerModule(module);
+        JsonResourceFactory factory = new JsonResourceFactory(getMapper());
+
+        return getMapper().writeValueAsString(resource);
+
+    }
+
+
+    public Object toObject(String jsonStr) throws IOException {
+        JsonNode actualObj = getMapper().readTree(jsonStr);
+
+        Object obj = mapper.reader()
+                .withAttribute(EMFContext.Attributes.RESOURCE, resource)
+                .forType(Object.class)
+                .readValue(actualObj);
+        return obj;
+    }
+}
