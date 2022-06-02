8dfb9ca9fb690fcf019060e0de4731220b43ece4
Chun <caochun@gmail.com>
2022-06-02 23:17:31 +0800

    Graphql to Cypher is awesome

--- src/main/java/info/nemoworks/grass/GSDLMeta.java
+++ src/main/java/info/nemoworks/grass/GSDLMeta.java
@@ -1,36 +1,33 @@
 package info.nemoworks.grass;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import org.neo4j.graphql.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 public class GSDLMeta {
 
-    private static String sdl = """
-            type Query {
-                bookById(id: ID): Book
-            }
 
-            type Book {
-                id: ID
-                name: String
-                pageCount: Int
-                author: Author
-            }
+    public static void main(String[] args) throws IOException, OptimizedQueryException {
+        URL url = Resources.getResource("person.graphql");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
 
-            type Author {
-                id: ID
-                firstName: String
-                lastName: String
-            }
-            """;
+        GraphQLSchema schema = SchemaBuilder.buildSchema(sdl);
+//        QueryContext ctx = new QueryContext();
+        String query = """
+                mutation {addPersonPets (  name: "abc", pets : "cat" ) {name}}
+                 """;
+        List<Cypher> c = new Translator(schema).translate(query);
 
-    public static void main(String[] args){
-        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
-        System.out.println(typeRegistry);
+        System.out.println(schema.getMutationType().getName());
+
     }
-
-
 
 
 }
